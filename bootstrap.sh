c8e0ff9c72479de7c4471c9d4d372c82470c1d4e
Xiao G. Wu <xiaogwu@gmail.com>
2013-01-04 17:56:03 -0800

    Corrected bootstrap.sh script and added README to conform with Github guidelines

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,19 @@
 DOTFILES=$HOME/dotfiles
 
 for file in $(ls -1 | grep -v "bootstrap.sh" | grep -v "README"); do
+    if [ -L "$HOME/.$file" ]; then
+        ln -siv "$DOTFILES/$file" "$HOME/.$file"
+        continue
+    fi
+
     if [ -f "$HOME/.$file" ]; then
         echo ".$file already exists in $HOME, renaming to $file.old"
         mv "$HOME/.$file" "$HOME/$file.old"
-    fi
-    if [ ! -L "$HOME/.$file" ]; then
         ln -sv "$DOTFILES/$file" "$HOME/.$file"
-    else
-        ln -siv "$DOTFILES/$file" "$HOME/.$file"
+        continue
     fi
+
+    ln -sv "$DOTFILES/$file" "$HOME/.$file"
 done
 
 exit 0
